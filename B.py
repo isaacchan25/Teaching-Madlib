c2687f4d9619e857d3df325ee3fe8af46127337b
Isaac Chan <isaacychan@gmail.com>
2023-09-25 16:29:20 -0400

    hello

--- B.py
+++ B.py
@@ -3,11 +3,11 @@
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
 number_between_1_and_9000 = 0 # assign some number or string at the end of each of these lines
-adjective_1 = ""
-noun_1 = ""
-famous_person = ""
-adjective_2 = ""
-very_large_number = 0
-noun_2 = ""
-adjective_2 = ""
-large_store = ""
+adjective_1 = "fat"
+noun_1 = "dog"
+famous_person = "iron man"
+adjective_2 = "hot"
+very_large_number = 73931020
+noun_2 = "man"
+adjective_2 = "funny"
+large_store = "walmart"
