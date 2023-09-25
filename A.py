ac842dc0c8c76b5a0c71c744ec5a32543f9c306f
Davidconfoey <25dconfoey@wpsstudents.org>
2023-09-25 16:31:46 -0400

    changed variables

--- A.py
+++ A.py
@@ -2,11 +2,11 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
+year_in_the_future = 2350 # assign numbers or strings to the end of these versions
+wacky_robot_name = "isaac"
+adjective_1 = "hard-working"
+violent_verb = "stab"
+tech_company = "apple"
 dollar_amount = ""
 adjective_2 = ""
 noun_1 = ""
